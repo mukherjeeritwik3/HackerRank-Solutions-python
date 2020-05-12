584c0e704204c3aaf00b204688e012c8b2028b69
mukherjeeritwik3 <65210468+mukherjeeritwik3@users.noreply.github.com>
2020-05-11 22:14:27 -0700

    Create Day_1.py

---
+++ 30 Days of Code/Day_1.py
@@ -0,0 +1,10 @@
+int_input = int(input())
+float_input = float(input())
+string = input()
+
+int_sum = int_input+i
+float_sum = float_input+d
+string_sum = s + string
+print(int_sum)
+print(float_sum)
+print(string_sum)
